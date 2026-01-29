5d35cd69a0e44d93f22337cc001a1d1cc8d490e9
Z <zhanarbek.a7@gmail.com>
2026-01-29 17:52:38 -0600

    Time: 3 ms (19.90%) | Memory: 52.6 MB (86.12%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int headASize = 1;
+        int headBSize = 1;
+
+        ListNode headALink = headA;
+        ListNode headBLink = headB;
+
+        while (headALink != null || headBLink != null) {
+            // This one will handle if the lists original size's are the same
+            // So meaning, their intersection will start at the same location for both
+            if (headALink != null && headBLink != null) {
+                if (headALink == headBLink) {
+                    return headALink;
+                }
+            }
+
+            if (headALink != null) {
+                if (headALink.next != null) {
+                    headASize++;
+                }
+                headALink = headALink.next;
+            }
+
+            if (headBLink != null) {
+                if (headBLink.next != null) {
+                    headBSize++;
+                }
+                headBLink = headBLink.next;
+            }
+        }
+
+        // below 2 conditionals are used to
+        if (headASize > headBSize) {
+            for (int i = 0; i < headASize - headBSize; i++) {
+                headA = headA.next;
+            }
+        } else {
+            for (int i = 0; i < headBSize - headASize; i++) {
+                headB = headB.next;
+            }
+        }
+
+        while (headA != null || headB != null) {
+            if (headA != null && headB != null) {
+                if (headA == headB) {
+                    return headA;
+                }
+            }
+            if (headA != null) {
+                headA = headA.next;
+            }
+
+            if (headB != null) {
+                headB = headB.next;
+            }
+        }
+
+        return null;
+    }
+}
