4f3dad4e4bbbb75c780787034bf2427a744b7cd9
Z <zhanarbek.a7@gmail.com>
2026-01-29 18:28:55 -0600

    Time: 1 ms (99.90%) | Memory: 53.2 MB (8.92%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,14 +11,51 @@
  */
 public class Solution {
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-    ListNode a = headA, b = headB;
+        int lenA = 0;
+        int lenB = 0;
 
-    while (a != b) {
-        a = (a == null) ? headB : a.next;
-        b = (b == null) ? headA : b.next;
+        ListNode headALink = headA;
+        ListNode headBLink = headB;
+
+        // Compute lengths
+        while (headALink != null) {
+            lenA++;
+            headALink = headALink.next;
+        }
+        while (headBLink != null) {
+            lenB++;
+            headBLink = headBLink.next;
+        }
+
+        headALink = headA;
+        headBLink = headB;
+
+        // Align heads
+        if (lenA > lenB) {
+            for (int i = 0; i < lenA - lenB; i++)
+                headALink = headALink.next;
+
+        } else {
+            for (int i = 0; i < lenB - lenA; i++)
+                headBLink = headBLink.next;
+        }
+
+        while (headALink != null || headBLink != null) {
+            if (headALink != null && headBLink != null) {
+                if (headALink == headBLink) {
+                    return headALink;
+                }
+            }
+            if (headALink != null) {
+                headALink = headALink.next;
+            }
+
+            if (headBLink != null) {
+                headBLink = headBLink.next;
+            }
+        }
+
+        return null;
     }
-
-    return a; // or b
-}
 
 }
